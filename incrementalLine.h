4fa4a332ac2bf19a2c01cd152ba82fe5c5b36bf1
EliDadd <45863805+EliDadd@users.noreply.github.com>
2019-01-04 23:51:16 -0500

    Lidar Stuff
    Working, have to make robust

--- incrementalLine.h
+++ incrementalLine.h
@@ -10,10 +10,16 @@ using namespace std;
 class line {
 public:
 	void setFloats();
+	//least squared fit
 	float getSlope();
+	//returns slope
 	float getIntercept();
+	//returns intercept
 	void addPoint(float xVal, float yVal);
 	float findDist(float xPoint, float yPoint);
+	void clearLine();
+	//empties the vectors of the line
+	void printLine();
 private:
 	vector <float> x;
 	vector <float> y;
@@ -27,6 +33,6 @@ private:
 //x includes all of the xpoints in the line
 //y includes all of the ypoints in the line
 
-void findLine(vector <float> xVal, vector <float> yVal);
+void findLine(vector <float> xReal, vector <float> yReal);
 
 #endif
